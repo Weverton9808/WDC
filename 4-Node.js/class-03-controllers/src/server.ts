043ef2ea7646408c6f983005c40ed08effc40708
weverton silva <wevertonsilv012@gmail.com>
2025-02-20 20:50:29 -0300

    add class-03-controllers

---
+++ 4-Node.js/class-03-controllers/src/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import { router } from "./routes";
+const app = express();
+
+const PORT = process.env.PORT || 3333;
+
+//middleware
+app.use(express.json());
+app.use(router);
+
+app.listen(PORT, ()=> {
+    console.log(`server is running on PORT  ${PORT}...`);
+});
